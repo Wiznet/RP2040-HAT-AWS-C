0e21ce0b847681f3b8aef142364c1250b6768619
GiungKim <forever4191@naver.com>
2022-03-14 10:27:33 +0900

    Add clock configuration function to examples

--- examples/aws_iot_http/aws_iot_http.c
+++ examples/aws_iot_http/aws_iot_http.c
@@ -28,6 +28,9 @@
  * Macros
  * ----------------------------------------------------------------------------------------------------
  */
+/* Clock */
+#define PLL_SYS_KHZ (133 * 1000)
+
 /* Buffer */
 #define ETHERNET_BUF_MAX_SIZE (1024 * 2)
 
@@ -70,6 +73,9 @@ static tlsContext_t g_http_tls_context;
  * Functions
  * ----------------------------------------------------------------------------------------------------
  */
+/* Clock */
+static void set_clock_khz(void);
+
 /* DHCP */
 static void wizchip_dhcp_init(void);
 static void wizchip_dhcp_assign(void);
@@ -84,6 +90,8 @@ int main()
 {
     /* Initialize */
     int retval = 0;
+
+    set_clock_khz();
 
     stdio_init_all();
 
@@ -141,6 +149,22 @@ int main()
  * Functions
  * ----------------------------------------------------------------------------------------------------
  */
+/* Clock */
+static void set_clock_khz(void)
+{
+    // set a system clock frequency in khz
+    set_sys_clock_khz(PLL_SYS_KHZ, true);
+
+    // configure the specified clock
+    clock_configure(
+        clk_peri,
+        0,                                                // No glitchless mux
+        CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, // System PLL on AUX mux
+        PLL_SYS_KHZ * 1000,                               // Input frequency
+        PLL_SYS_KHZ * 1000                                // Output (must be same as no divider)
+    );
+}
+
 /* DHCP */
 static void wizchip_dhcp_init(void)
 {
--- examples/aws_iot_mqtt/aws_iot_mqtt.c
+++ examples/aws_iot_mqtt/aws_iot_mqtt.c
@@ -31,6 +31,9 @@
  * Macros
  * ----------------------------------------------------------------------------------------------------
  */
+/* Clock */
+#define PLL_SYS_KHZ (133 * 1000)
+
 /* Buffer */
 #define ETHERNET_BUF_MAX_SIZE (1024 * 2)
 
@@ -88,6 +91,9 @@ tlsContext_t g_mqtt_tls_context;
  * Functions
  * ----------------------------------------------------------------------------------------------------
  */
+/* Clock */
+static void set_clock_khz(void);
+
 /* DHCP */
 static void wizchip_dhcp_init(void);
 static void wizchip_dhcp_assign(void);
@@ -105,6 +111,8 @@ int main()
     uint32_t tick_start = 0;
     uint32_t tick_end = 0;
     uint32_t pub_cnt = 0;
+
+    set_clock_khz();
 
     stdio_init_all();
 
@@ -214,6 +222,22 @@ int main()
  * Functions
  * ----------------------------------------------------------------------------------------------------
  */
+/* Clock */
+static void set_clock_khz(void)
+{
+    // set a system clock frequency in khz
+    set_sys_clock_khz(PLL_SYS_KHZ, true);
+
+    // configure the specified clock
+    clock_configure(
+        clk_peri,
+        0,                                                // No glitchless mux
+        CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, // System PLL on AUX mux
+        PLL_SYS_KHZ * 1000,                               // Input frequency
+        PLL_SYS_KHZ * 1000                                // Output (must be same as no divider)
+    );
+}
+
 /* DHCP */
 static void wizchip_dhcp_init(void)
 {
